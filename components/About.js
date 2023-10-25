c71e589e1fdde3a9dd999550a04572d17c344707
ruthanggriani <145457873+ruthanggriani@users.noreply.github.com>
2023-10-25 16:31:18 +0800

    Add files via upload

---
+++ components/About.js
@@ -0,0 +1,17 @@
+//about page
+function About () {
+	return(
+		<div>
+			<h2>Front-End web development is the developmentof the graphical
+			user interface of a website, through the use of HTML, CSS, and
+			JavaScript, so that users can view and interact with that website.
+			</h2>
+			
+			Read more the article :
+			<a href=
+			"https://frontendmasters.com/guides/front-end-handbook/2019/">
+				https://frontendmasters.com/guides/front-end-handbook/2019/
+			</a>
+		</div>
+	);
+}
